7de2761609af14d4f538f0d506e4e1099b337009
creamsandwch <neuronrus1996@gmail.com>
2022-11-16 16:22:09 +0500

    assertRedirects in test_forms - doesnt work properly, gets 302 instead of 200

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,4 +1,50 @@
-from django.test import TestCase
-from django.urls
+from django.test import TestCase, Client
+from django.urls import reverse
 from posts.forms import PostForm
 from posts.models import Post, Group, User
+
+
+class PostFormTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.group = Group.objects.create(
+            title='Тестовое название группы',
+            slug='test-slug',
+        )
+        cls.author = User.objects.create_user(username='TestUser')
+        cls.post = Post.objects.create(
+            text='Текст тестового поста',
+            author=cls.author,
+            group=cls.group
+        )
+        cls.authorized_client = Client()
+        cls.authorized_client.force_login(cls.author)
+        cls.form = PostForm()
+
+    def test_post_form_create(self):
+        """Валидная форма PostForm создаёт запись в БД."""
+        posts_count = Post.objects.count()
+        form_data = {
+            'text': 'Текст тестового поста из формы',
+            'group': PostFormTests.group,
+        }
+        response = PostFormTests.authorized_client.post(
+            reverse('posts:post_create'),
+            data=form_data,
+            follow=True,
+        )
+        self.assertRedirects(
+            response,
+            reverse(
+                'posts:profile',
+                kwargs={'username': PostFormTests.author.username}
+            )
+        )
+        self.assertEqual(Post.objects.count(), posts_count + 1)
+        self.assertTrue(
+            Post.objects.filter(
+                text='Текст тестового поста из формы',
+                group=PostFormTests.group,
+            ).exists()
+        )
